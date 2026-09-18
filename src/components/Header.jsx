6416f7add1cb2cca54140dd025c8c00914389889
francourage56-byte <francourage56@gmail.com>
2026-09-18 11:56:39 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,50 @@
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 w-full h-[72px] bg-[#090d16]/85 backdrop-blur-x1 border-b border-gray-800 z-50 transition-all">
+      <div className="max-w-7x1 mx-auto h-full px-6 flex justify-between items-center">
+
+        {/* brand logo */}
+        <a
+          href="#"
+          className="flex items-center gap-2 text-xl font-bold tracking-tight text-white group"
+        >
+          <div className="w-8 h-8 rounded-lg bg-gradient-to-tr from-indigo-500 to-purple-400 flex items-center justify-center">
+            A
+          </div>
+          <span>
+            Apex <span className="text-indigo-400">Engine</span>
+          </span>
+        </a>
+
+    {/* Nav Links */}
+    <nav>
+      <ul className="flex items-center gap-1 sm:gap-6 text-xs sm:texts-sm font-medium text-gray-500">
+        <li>
+          <a href="#features" className="hover:text-white hover:bg-gray-800/60 px-3 py-2 rounded-lg transition-colors">Modules</a>
+        </li>
+
+         <li>
+          <a href="#workflow" className="hover:text-white hover:bg-gray-800/60 px-3 py-2 rounded-lg transition-colors">Workflow</a>
+        </li>
+         <li>
+          <a href="#architecture" className="hover:text-white hover:bg-gray-800/60 px-3 py-2 rounded-lg transition-colors">Architecture</a>
+        </li>
+         <li>
+          <a href="#pricing" className="hover:text-white hover:bg-gray-800/60 px-3 py-2 rounded-lg transition-colors">Pricing</a>
+        </li>
+         <li>
+          <a href="#faq" className="hover:text-white hover:bg-gray-800/60 px-3 py-2 rounded-lg transition-colors ">FAQ</a>
+        </li>
+      </ul>
+    </nav>
+
+{/* Action botton */}
+<a href="#pricing" className="hidden lg:inline-flex items-center justify-center bg-indigo-600 hover:bg-indigo-500 texts-white font-semibold text-xs uppercase tracking-wider px-4 py-2 rounded-lg transition-all shadow-md shadow-indigo-500/20">Deploy Now</a>
+    
+        
+      </div>
+    </header>
+  );
+};
+
+export default Header;
